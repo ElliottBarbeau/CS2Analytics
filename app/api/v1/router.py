fed614a977c153b3f62e1b8be2c32a8c2e58ef5e
ElliottBarbeau <elliottbarbeau@gmail.com>
2026-03-10 15:52:29 -0400

    remove references to elo/jobs

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
 from app.api.v1.routes.players import router as players_router
-from app.api.v1.routes.jobs import router as jobs_router
-from app.api.v1.routes.elo import router as elo_router
 from app.api.v1.routes.teams import router as teams_router
 from app.api.v1.routes.team_map_stats import router as team_map_stats_router
 from app.api.v1.routes.teams_lookup import router as teams_lookup_router
@@ -13,8 +11,6 @@ from app.api.v1.routes.debug_match_stats import router as debug_match_stats_rout
 
 api_router = APIRouter()
 api_router.include_router(players_router)
-api_router.include_router(jobs_router)
-api_router.include_router(elo_router)
 api_router.include_router(teams_router)
 api_router.include_router(team_map_stats_router)
 api_router.include_router(teams_lookup_router)
